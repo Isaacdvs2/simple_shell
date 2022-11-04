cdf39c210dd015056e329d0b8e72dd5a6376d4ff
Isaacdvs2 <dvs2mona@gmail.com>
2022-11-04 02:02:53 -0700

    added readme, header file and main prog

---
+++ bbc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - this is the main entry function of a c program.
+ * @void: takes no args
+ *
+ * Description: just a simple code that passes the Betty checker.
+ * Return: 0 for success.
+ */
+
+int main(void)
+{
+
+return (0)
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * This header file will store all simple shell function
+ * prototypes and structs.
+ * */
+
+#endif
